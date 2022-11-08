7fd149483d94d1f37548900b08e30259143519d7
Crystal Korth <101788833+defworm@users.noreply.github.com>
2022-11-07 20:20:25 -0600

    error checking

---
+++ error_checking.py
@@ -0,0 +1,60 @@
+# acivity for errors
+# fileName = input("Please enter the name of the file you would like to read: ")
+# myfile = open(fileName, 'r') # Open a file for reading.
+# contents = myfile.readlines() # Read in the content by line.
+# myfile.close() # Explicitly close the file
+# print(contents) #print the contents of the file
+
+class FileManipulator:
+    def __init__(self, fileName):
+        self.contents = None
+        while self.contents == None:
+            try:
+                myfile = open(fileName, 'r')
+                self.contents = myfile.readlines()
+            except (FileNotFoundError, TypeError, OSError) as e:
+                print(type(e), e)
+                fileName = input("Please enter a valid file name: ")
+            else:
+                myfile.close()
+
+    def reverse(self, fileName):
+        while True:
+            try:
+                myfile = open(fileName, 'w')
+                revContent = [x.strip()[::-1] for x in self.contents]
+                for i in range(len(revContent)):
+                    myfile.write(revContent[i])
+                else:
+                    myfile.write(revContent[i] + '\n')
+            except (FileNotFoundError, TypeError, OSError) as e:
+                print(type(e),e)
+                fileName = input ("Please enter a valid file name: ")
+            else:
+                myfile.close()
+                break
+ #bonus
+    def upper(self,fileName):
+         while True:
+            try:
+                myfile = open(fileName, 'w') # Open a file for writing
+                upperContent = [x.strip().upper() for x in self.contents]
+                for i in range(len(upperContent)):
+                    if i == (len(upperContent)-1):
+                        myfile.write(upperContent[i])
+                    else:
+                        myfile.write(upperContent[i] + '\n') # write desired output to file
+            except (FileNotFoundError, TypeError, OSError) as e:
+                print(type(e),e)
+                fileName = input("Please enter a valid file name: ")
+            else:
+                myfile.close()
+                break
+
+
+# test code:
+
+f = FileManipulator("tmp.txt")
+print(f.contents)
+f.reverse("tmp2.txt")
+print("This is the end")
